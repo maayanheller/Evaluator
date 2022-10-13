a4c2662896f42409f79c380dd38d85c4ecbdf925
Maayan Heller <maayan.heller@gmail.com>
2022-10-14 01:00:56 +0300

    Initial state

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -11,17 +11,9 @@ export const App = () => {
   }, []);
 
   return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to client!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
-      </div>
-      <div>{m.message}</div>
-    </>
+    <div>
+      
+    </div>
   );
 };
 
